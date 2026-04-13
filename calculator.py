a81d3c4b444511ba5b1f846c480720c8267a9303
nahomicn <naomicarrazanan@gmail.com>
2026-04-13 19:34:22 -0400

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,35 @@
+# https://github.com/nahomicn/lab11-NC-NA
+# Partner 1: Nahomi Carrazana
+# Partner 2: N/A (No partner assigned)
+
+import math
+
+def square_root(a):
+    if a < 0:
+        raise ValueError
+    return math.sqrt(a)
+
+def hypotenuse(a, b):
+    return math.hypot(a, b)
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if a == 0:
+        raise ZeroDivisionError
+    return b / a
+
+def logarithm(a, b):
+    if a <= 0 or a == 1 or b <= 0:
+        raise ValueError
+    return math.log(b, a)
+
+def exponent(a, b):
+    return a ** b
